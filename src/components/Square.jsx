b946bdefd62f0a6ea5396fef49e031f00e17a6d9
gamal2001 <kmhamak@gmail.com>
2023-09-04 17:42:24 +0300

    add Board

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,9 @@
-import "./Square.css";
 function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
